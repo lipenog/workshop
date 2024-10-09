8e2c22b72280388039cf70aa05979b8defa0c1b9
lipenog <luizfenogueira@outlook.com>
2024-10-09 19:33:43 -0300

    added orders dto validator

--- src/main/java/com/example/productproject/web/controller/OrdersController.java
+++ src/main/java/com/example/productproject/web/controller/OrdersController.java
@@ -1,12 +1,22 @@
 package com.example.productproject.web.controller;
 
+import com.example.productproject.exception.InvalidDtoException;
 import com.example.productproject.web.dto.OrdersDTO;
+import com.example.productproject.web.dto.OrdersItemDTO;
+import com.example.productproject.web.dto.ProductsDTO;
 import com.example.productproject.web.service.ProductsService;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 public class OrdersController {
@@ -18,7 +28,20 @@ public class OrdersController {
     }
 
     @PostMapping("/orders")
-    public ResponseEntity<String> createOrder(@RequestBody OrdersDTO ordersDTO){
+    public ResponseEntity<String> createOrder(@RequestBody OrdersDTO ordersDTO) throws InvalidDtoException {
+        List<String> violations = verifyDTO(ordersDTO);
+        if(!violations.isEmpty()){
+            throw new InvalidDtoException(violations);
+        }
+        return ResponseEntity.ok("a");
+    }
 
+    private List<String> verifyDTO(OrdersDTO ordersDTO){
+        Validator validator;
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+            Set<ConstraintViolation<OrdersDTO>> violations = validator.validate(ordersDTO);
+            return violations.stream().map(ConstraintViolation::getMessage).toList();
+        }
     }
 }
--- src/main/java/com/example/productproject/web/dto/OrdersDTO.java
+++ src/main/java/com/example/productproject/web/dto/OrdersDTO.java
@@ -1,6 +1,7 @@
 package com.example.productproject.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,5 +12,6 @@ import java.util.List;
 @Getter
 public class OrdersDTO {
     @Size(min = 1, message = "orders.error.quantity")
+    @Valid // TODO remember that this tag makes the validator catch inside orders item dto
     private List<OrdersItemDTO> products;
 }
