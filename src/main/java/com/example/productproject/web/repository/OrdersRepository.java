b71678640678b4040bb78c5f3be410696b6e71f5
lipenog <luizfenogueira@outlook.com>
2024-10-10 06:46:44 -0300

    added orders repository

---
+++ src/main/java/com/example/productproject/web/repository/OrdersRepository.java
@@ -0,0 +1,9 @@
+package com.example.productproject.web.repository;
+
+import com.example.productproject.web.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+}
